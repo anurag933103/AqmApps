1b4b73acfeb4dfa061e76d93710846f3a2172c53
anurag933103 <anurag933103@gmail.com>
2020-06-22 08:18:20 -0700

    task1 done

--- src/SimplePumpSubscriber.java
+++ src/SimplePumpSubscriber.java
@@ -1,10 +1,21 @@
 import javax.jms.*;
 import javax.naming.*;
 import org.apache.log4j.BasicConfigurator;
+
+import edu.asupoly.heal.aqm.dmp.AQMDAOFactory;
+import edu.asupoly.heal.aqm.dmp.IAQMDAO;
+import edu.asupoly.heal.aqm.model.ServerPushEvent;
+
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.StreamCorruptedException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class SimplePumpSubscriber implements javax.jms.MessageListener{
+	private static Logger log = Logger.getLogger(SimplePumpSubscriber.class.getName());
 	private TopicSession pubSession;
     private TopicConnection connection;
     
@@ -33,12 +44,43 @@ public class SimplePumpSubscriber implements javax.jms.MessageListener{
                 if (message instanceof TextMessage) {
                     TextMessage txtMessage = (TextMessage) message;
                     System.out.println("Message received: " + txtMessage.getText());
+                    processJson(txtMessage.getText());
                 } else {
                     System.out.println("Invalid message received.");
                 }
             } catch (JMSException e1) {
     	    e1.printStackTrace();
     	}
+       }
+       
+       public void processJson(String jsonString) {
+    	   int appReturnValue = ServerPushEvent.PUSH_UNSET;
+    	   char firstBracket = '[';
+    	   char secondBracket = ']';
+    	   jsonString = firstBracket + jsonString;
+    	   jsonString = jsonString + secondBracket;
+    	   try {
+    		   log.info("Getting Dao Object ");
+			   IAQMDAO dao = AQMDAOFactory.getDAO();
+			   log.info(" Dao Object is "+dao);
+			   appReturnValue = (dao.importReadings(jsonString)) ? ServerPushEvent.SERVER_PUSH_OK : ServerPushEvent.SERVER_IMPORT_FAILED;
+    	   }catch (StreamCorruptedException sce) {
+    	       log.log(Level.SEVERE, "Server pushed stacktrace on response: " + sce);
+    	       appReturnValue = ServerPushEvent.SERVER_STREAM_CORRUPTED_EXCEPTION;
+   		   } catch (IOException ie) {
+   			   log.log(Level.SEVERE, "Server pushed stacktrace on response: " + ie);
+   			   appReturnValue = ServerPushEvent.SERVER_IO_EXCEPTION;
+   		   } catch (SecurityException se) {
+   			   log.log(Level.SEVERE, "Server pushed stacktrace on response: " + se);
+   			   appReturnValue = ServerPushEvent.SERVER_SECURITY_EXCEPTION;
+   		   } catch (NullPointerException npe) {
+   			   log.log(Level.SEVERE, "Server pushed stacktrace on response: " + npe);
+   			   appReturnValue = ServerPushEvent.SERVER_NULL_POINTER_EXCEPTION;
+   		   } catch (Throwable t) {
+   			   log.log(Level.SEVERE, "Server pushed stacktrace on response: " + t);
+   			   appReturnValue = ServerPushEvent.SERVER_UNKNOWN_ERROR;
+   		   }
+   			log.info("Server returning value to subscriber: " + appReturnValue);
        }
         
         public static void main(String[] args) {
